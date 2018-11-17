51d27402c73756640094bc5c474820f6eba41289
Michael Skovorodin <17948327+MorandiGonzalez@users.noreply.github.com>
2018-11-17 19:30:02 +0200

    Update Main.c

--- OS_tasks/Labs/Lab_2/Main.c
+++ OS_tasks/Labs/Lab_2/Main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 
+//Функция которая возвращает значение заданной функции
 double function(double x)
 {
 	return 1/(1-cos(x));
@@ -10,19 +11,22 @@ void main() {
 	
 	float a, b, n;
 
+	//Вводим границу a
 	printf("Input a = ");
 	scanf("%f",&a);
-
+	
+	//Вводим границу b
 	printf("Input b = ");
 	scanf("%f", &b);
-
+	
+	//Вводим шаг n
 	printf("Input n = ");
 	scanf("%f", &n);
 
 	printf("%f", a);
 
 	
-
+	//Вычисляем функцию с заданными параметрами
 	for (float i = a; i <= b; i += n) {
 		printf( "\nF(x) = %f ", function(i) );
 
